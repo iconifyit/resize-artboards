ea3123ab567fa3e8e73500883596a8bb27348751
Scott Lewis <scott@iconify.it>
2018-12-05 17:33:32 -0500

    Update to latest code version

--- Resize Artboards.jsx
+++ Resize Artboards.jsx
@@ -2,41 +2,59 @@
  * Based on the script resizeArtboards_CS4andUp.jsx by Carlos Canto 11/4/12
  */
 
-#target Illustrator  
+#target Illustrator
 
 var OPTIONS = {
-    size: 32
+    size   : 32,
+    width  : 32,
+    height : 32
+};
+
+if (app.documents.length > 0) {
+
+    var idoc  = app.activeDocument;
+    var title = "Resize All Artboards";
+
+    OPTIONS.size = Window.prompt ("Enter New Artboard size in pixels as WxH ( Example: 250x300 )", 32, title);
+
+    if (OPTIONS.size.indexOf('x') != -1) {
+        var bits = OPTIONS.size.split('x');
+        OPTIONS.width  = parseInt(bits[0]);
+        OPTIONS.height = parseInt(bits[1]);
+    }
+    else {
+        OPTIONS.width  = OPTIONS.size;
+        OPTIONS.height = OPTIONS.size;
+    }
+
+    try {
+        var width  = OPTIONS.width;
+        var height = OPTIONS.height;
+
+        for (i=0; i<idoc.artboards.length; i++) {
+            var abBounds = idoc.artboards[i].artboardRect; // left, top, right, bottom
+
+            var ableft   = abBounds[0];
+            var abtop    = abBounds[1];
+            var abwidth  = abBounds[2] - ableft;
+            var abheight = abtop- abBounds[3];
+
+            var abctrx   = abwidth / 2 + ableft;
+            var abctry   = abtop - abheight / 2;
+
+            var ableft   = abctrx - width  / 2;
+            var abtop    = abctry + height / 2;
+            var abright  = abctrx + width  / 2;
+            var abbottom = abctry - height / 2;
+
+            idoc.artboards[i].artboardRect = [ableft, abtop, abright, abbottom];
+        }
+    }
+    catch(e) {
+        alert(e.message);
+        /** Exist gracfully for now */
+    }
 }
-
-if (app.documents.length > 0) {  
-
-    var idoc  = app.activeDocument;  
-    var title = "Resize All Artboards";  
-
-    OPTIONS.size = Number(Window.prompt ("Enter New Artboard size in pixels (W & H)", 32, title));
-  
-    var width  = OPTIONS.size;
-    var height = OPTIONS.size;
-  
-    for (i=0; i<idoc.artboards.length; i++) {  
-        var abBounds = idoc.artboards[i].artboardRect;// left, top, right, bottom  
-  
-        var ableft = abBounds[0]; // 0  
-        var abtop = abBounds[1]; // 612  
-        var abwidth = abBounds[2] - ableft; // 792 // width  
-        var abheight = abtop- abBounds[3]; // 0 // height  
-          
-        var abctrx = abwidth/2+ableft;  
-        var abctry = abtop-abheight/2;  
-               
-        var ableft = abctrx-width/2;   
-        var abtop = abctry+height/2;  
-        var abright = abctrx+width/2;   
-        var abbottom = abctry-height/2;  
-          
-        idoc.artboards[i].artboardRect = [ableft, abtop, abright, abbottom];  
-    }  
-}  
-else  {  
-    alert ("there are no open documents");  
+else  {
+    alert ("there are no open documents");
 } 
